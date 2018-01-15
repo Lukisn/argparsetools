8a13a4c7250a38500c83e1716d8ae8a5a036dccc
Lukas Reimer <lukas.reimer@icloud.com>
2018-01-15 07:47:19 +0100

    added core package and initial empty modules

---
+++ argparsetools/actions.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Custom actions for use with argparse's action keyword argument to the
+add_argument method.
+"""
---
+++ argparsetools/types.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Type checking functions for use with arparse's type keyword argument to
+the add_argument method.
+"""
