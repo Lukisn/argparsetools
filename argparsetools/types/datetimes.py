cb0aa9bb9488f66dd81db6b34e0d0ffb5d740b50
Lukas Reimer <lukas.reimer@icloud.com>
2018-02-14 08:12:56 +0100

    added idea for datetime related type checking

---
+++ argparsetools/types/datetimes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Type checking functions for datetime related types for use with arparse."""
+
+from argparse import ArgumentTypeError
+
+
+def datetime_string(string, format):
+    raise NotImplementedError
